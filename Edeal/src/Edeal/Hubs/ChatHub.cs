0bae9952f83088a014a38524022408b01c7fa372
Khiem <buithen.khiem2717@gmail.com>
2015-12-04 01:04:27 +0700

    start up project

---
+++ Edeal/src/Edeal/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Cors.Core;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System.Threading.Tasks;
+
+namespace Edeal.Hubs
+{
+    [HubName("chatHub")]
+    [EnableCors("AllowAll")]
+    public class ChatHub : Hub
+    {
+        public async Task BroadcastMessage(string groupName, string callerName, string message)
+        {
+            // Case-insensitive when the server RPC the client's methods
+            await Clients.All.appendNewMessage(callerName, message);
+        }
+    }
+}
---
+++ Edeal/src/Edeal/ITestService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Edeal
+{
+    public interface ITestService
+    {
+        int getdata();
+    }
+}
